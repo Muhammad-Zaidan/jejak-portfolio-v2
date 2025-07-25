fa3eec62ee09392f6b7082cece326966763d85dc
Muhammad Zaidan <babacokis@gmail.com>
2025-07-25 17:28:49 +0700

    menghapus file notion 2

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/projects/page.tsx
-import { getProjects } from "@/lib/notion";
-import Projects from "@/Components/layout/Projects";
-
-export default async function ProjectsPage() {
-  const projects = await getProjects(); // ini fetch server-side
-
-  return <Projects projects={projects} />;
-}
